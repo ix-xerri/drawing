3bf426875a5cc0e61a8f7bff5e1b1da98daed3d2
ix-xerri <matthewxerri@Matthews-MacBook-Pro.local>
2014-10-27 21:19:05 +0100

    Saving the participants on the server side

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var drawing = {
     clickDrag: []
 };
 
+var users = {}
+
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
 
@@ -18,6 +20,22 @@ http.listen(app.get('port'), function() {
 });
 
 io.on('connection', function(socket){
+    //log new user
+    var address = socket.handshake.address,
+        userAgent = socket.handshake.headers['user-agent'];
+    
+    console.log("New connection from " + address + " - " + userAgent);
+    
+    //save new user to list
+    users[socket.id] = {
+        address: address,
+        userAgent: userAgent
+    }
+    
+    //send new user list
+    io.emit('users', users);
+    
+    //emit entire drawing
     io.emit('draw', drawing);
     
     socket.on('draw', function(params) {
@@ -26,5 +44,14 @@ io.on('connection', function(socket){
         
         //emit the entire drawing
         io.emit('draw',params);
+    });
+    
+    socket.on('disconnect', function() { 
+        console.log(socket.id + ' disconnected');
+        //delete from the user store
+        delete users[socket.id];
+        
+        //send new user list
+        io.emit('users', users);
     });
 });
