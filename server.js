6fce75755730ab464e50f32301b54970b928d7d1
ix-xerri <matthewxerri@Matthews-MacBook-Pro.local>
2014-10-27 20:41:19 +0100

    Canvas able to draw

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(__dirname + '/public'));
+
+http.listen(app.get('port'), function() {
+    console.log('open localhost:'+app.get('port'));
+});
